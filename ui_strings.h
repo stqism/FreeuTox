afef64e6e248ae5300a6457bc83fc35cc15e6432
Sean Qureshi <sean@tox.im>
2014-07-14 10:03:03 -0700

    Standardize locale a bit, forgot to push ui_strings

--- ui_strings.h
+++ ui_strings.h
@@ -1,1 +1,39 @@
-ui_strings.en_us.h
+#include<libintl.h>
+#include<locale.h>
+
+#define AFS(x) { .str = (uint8_t*)x, .length = sizeof(x) - 1 }
+#define _(String) gettext (String)
+
+static STRING addstatus[] = {
+    AFS(_("Friend request sent. Your friend will appear online after accepting it.")),
+    AFS(_("Attempting to resolve DNS name...")),
+    AFS(_("Error: Invalid Tox ID")),
+    AFS(_("Error: No Tox ID specified")),
+    AFS(_("Error: Message is too long")),
+    AFS(_("Error: Empty message")),
+    AFS(_("Error: Tox ID is self ID")),
+    AFS(_("Error: Tox ID is already in friend list")),
+    AFS(_("Error: Unknown")),
+    AFS(_("Error: Invalid Tox ID (bad checksum)")),
+    AFS(_("Error: Invalid Tox ID (bad nospam value)")),
+    AFS(_("Error: No memory"))
+};
+
+STRING filestatus[] = {
+    AFS(_("New file transfer")),
+    AFS(_("File transfer started")),
+    AFS(".."),
+    AFS(_("File transfer paused")),
+    AFS(_("File transfer broken")),
+    AFS(_("File transfer cancelled")),
+    AFS(_("File transfer complete"))
+};
+
+STRING callstatus[] = {
+    AFS(_("Call cancelled")),
+    AFS(_("Call invited")),
+    AFS(_("Call ringing")),
+    AFS(_("Call started"))
+};
+
+#undef AFS
